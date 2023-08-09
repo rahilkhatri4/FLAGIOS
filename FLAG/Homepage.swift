ba22d437ee1e1520019f148e0783aec451902013
rahilkhatri4 <rahilkhatri4@gmail.com>
2023-08-09 16:49:47 -0400

    Integrated API with homepage

--- FLAG/Homepage.swift
+++ FLAG/Homepage.swift
@@ -42,5 +42,40 @@ class HomepageController: UIViewController {
         interestView.layer.cornerRadius = 5;
         interestView.layer.masksToBounds = true;
         
+        fetchUsers()
+        
+        }
+    
+    func fetchUsers() {
+            guard let url = URL(string: "http://localhost/flag/getUsers.php") else {
+                print("Invalid URL")
+                return
+            }
+            
+            URLSession.shared.dataTask(with: url) { data, response, error in
+                if let error = error {
+                    print("Error: \(error.localizedDescription)")
+                    return
+                }
+                
+                if let data = data {
+                    if let jsonString = String(data: data, encoding: .utf8) {
+                        // Print the JSON response (encoded string)
+                        print("JSON Response:\n\(jsonString)")
+                        
+                        // Parse the JSON-encoded string
+                        if let jsonData = jsonString.data(using: .utf8),
+                           let jsonDictionary = try? JSONSerialization.jsonObject(with: jsonData, options: []) as? [String: Any],
+                           let success = jsonDictionary["success"] as? Bool, success,
+                           let usersArray = jsonDictionary["users"] as? [[String: Any]] {
+                            
+                            // Update the UI on the main thread
+//                            DispatchQueue.main.async {
+//                                self.displayUsers(usersArray)
+//                            }
+                        }
+                    }
+                }
+            }.resume()
         }
     }
