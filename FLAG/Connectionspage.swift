5874d343f72ac81c2d86401d53bb1ecd0477df04
rahilkhatri4 <rahilkhatri4@gmail.com>
2023-08-09 16:27:29 -0400

    Integrated connections page

--- FLAG/Connectionspage.swift
+++ FLAG/Connectionspage.swift
@@ -1,11 +1,3 @@
-//
-//  Connectionspage.swift
-//  FLAG
-//
-//  Created by VAMSI KATABATHUNI on 2023-07-25.
-//
-
-import Foundation
 import UIKit
 
 class ConnectionspageController: UIViewController {
@@ -20,7 +12,6 @@ class ConnectionspageController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-
         // Set the content mode to aspect fit (or any other mode you prefer)
         profilePicture.contentMode = .scaleToFill
         
@@ -34,6 +25,61 @@ class ConnectionspageController: UIViewController {
         studentIdLabel.applyGradientTextColor(gradientObject: gradientObject)
         
         // Set the font size for usernameLabel
-               usernameLabel.font = UIFont.systemFont(ofSize: 20, weight: .bold)
+        usernameLabel.font = UIFont.systemFont(ofSize: 20, weight: .bold)
+        
+        fetchUsers()
+    }
+    
+    func fetchUsers() {
+        guard let url = URL(string: "http://localhost/flag/getUsers.php") else {
+            print("Invalid URL")
+            return
+        }
+        
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            if let error = error {
+                print("Error: \(error.localizedDescription)")
+                return
+            }
+            
+            if let data = data {
+                if let jsonString = String(data: data, encoding: .utf8) {
+                    // Print the JSON response (encoded string)
+                    print("JSON Response:\n\(jsonString)")
+                    
+                    // Parse the JSON-encoded string
+                    if let jsonData = jsonString.data(using: .utf8),
+                       let jsonDictionary = try? JSONSerialization.jsonObject(with: jsonData, options: []) as? [String: Any],
+                       let usersArray = jsonDictionary["users"] as? [[String: Any]] {
+                        
+                        // Update the UI on the main thread
+                        DispatchQueue.main.async {
+                            self.displayUsers(usersArray)
+                        }
+                    }
+                }
+            }
+        }.resume()
+    }
+    
+    func displayUsers(_ jsonArray: [[String: Any]]) {
+        // Clear existing subviews
+//        for subview in gradientView.subviews {
+//            subview.removeFromSuperview()
+//        }
+
+        // Iterate through the array of users and add labels to display user information
+        var yOffset: CGFloat = 200.0
+        for user in jsonArray {
+            let name = user["name"]!
+           let birthdate = user["birthdate"]!
+            print(name)
+            print(birthdate)
+//            let userLabel = UILabel()
+//            userLabel.text = "Name: \(name), Birthdate: \(birthdate)"
+//            userLabel.frame = CGRect(x: 20, y: yOffset, width: 300, height: 30)
+//            gradientView.addSubview(userLabel)
+//            yOffset += 40.0 // Adjust the vertical spacing
+        }
     }
 }
