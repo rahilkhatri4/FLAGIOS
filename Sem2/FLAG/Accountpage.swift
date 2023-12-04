6aecbb24967f11d4311b3c50efcab739711f35ab
rahilkhatri4 <rahilkhatri4@gmail.com>
2023-12-04 19:03:20 +0530

    Made Update page working

--- Sem2/FLAG/Accountpage.swift
+++ Sem2/FLAG/Accountpage.swift
@@ -13,8 +13,6 @@ class AccountpageController: UIViewController, UIPickerViewDelegate, UIPickerVie
 
     @IBOutlet weak var userTextField: UITextField!
     
-    @IBOutlet weak var mobileTextField: UITextField!
-    
     @IBOutlet weak var aboutTextField: UITextField!
         
     @IBOutlet weak var FromAge: UITextField!
@@ -44,6 +42,52 @@ class AccountpageController: UIViewController, UIPickerViewDelegate, UIPickerVie
 //       }
     
     
+    @IBAction func updateButtonClicked(_ sender: UIButton) {
+        guard let ID = getUserID() else {
+            print("User ID not found")
+            return
+        }
+
+        let updateUrlString = "https://e-invite.site/updateUser.php"
+        guard let updateUrl = URL(string: updateUrlString) else {
+            print("Invalid update URL")
+            return
+        }
+
+        var request = URLRequest(url: updateUrl)
+        request.httpMethod = "POST"
+        request.addValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
+
+        // Create URL-encoded parameters
+        let updatePostString = "id=\(ID)" +
+            "&name=\(userTextField.text ?? "")" +
+            "&bio=\(aboutTextField.text ?? "")" +
+            "&fromage=\(FromAge.text ?? "")" +
+            "&toage=\(ToAge.text ?? "")" +
+            "&birthdate=\(dateFormatter.string(from: dobPicker.date))" +
+            "&gender=\(genderData[genderPicker.selectedRow(inComponent: 0)])" +
+            "&preferred_gender=\(genderData[interestedPicker.selectedRow(inComponent: 0)])" +
+            "&campus=\(campusOptions[campusPicker.selectedRow(inComponent: 0)])"
+
+        request.httpBody = updatePostString.data(using: .utf8)
+
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            if let error = error {
+                print("Error updating user: \(error.localizedDescription)")
+                return
+            }
+
+            if let data = data {
+                if let dataString = String(data: data, encoding: .utf8) {
+                    print("Update response:\n\(dataString)")
+
+                    // Handle the update response as needed
+                    // You might want to check for success or show an alert to the user
+                }
+            }
+        }.resume()
+    }
+
     @IBAction func gobackButtonTapped(_ sender: UIButton) {
         // Navigate back to the previous view controller
         navigationController?.popViewController(animated: true)
@@ -67,12 +111,10 @@ class AccountpageController: UIViewController, UIPickerViewDelegate, UIPickerVie
               
         // Apply the corner radius
                 userTextField.layer.cornerRadius = 25
-                mobileTextField.layer.cornerRadius = 25
         
         
         // Apply border to text fields
         userTextField.applyGradientBorderColor(gradientObject: gradientObject, cornerRadius: 10, padding: UIEdgeInsets())
-        mobileTextField.applyGradientBorderColor(gradientObject: gradientObject, cornerRadius: 10, padding: UIEdgeInsets())
 //        intrestTextField.applyGradientBorderColor(gradientObject: gradientObject, cornerRadius: 10, padding: UIEdgeInsets())
 //        birthdayTextField.applyGradientBorderColor(gradientObject: gradientObject, cornerRadius: 10, padding: UIEdgeInsets())
 //        CampusTextField.applyGradientBorderColor(gradientObject: gradientObject, cornerRadius: 10, padding: UIEdgeInsets())
@@ -136,11 +178,39 @@ class AccountpageController: UIViewController, UIPickerViewDelegate, UIPickerVie
             }
         }
 
-        func updateUI(with user: [String: Any]) {
-            // Update UI elements with user details
-            userTextField.text = user["name"] as? String
-            // Update other fields similarly
+    let dateFormatter: DateFormatter = {
+            let formatter = DateFormatter()
+            formatter.dateFormat = "yyyy-MM-dd" // Adjust the date format based on your API response
+            return formatter
+        }()
+    
+    func updateUI(with user: [String: Any]) {
+        // Update UI elements with user details
+        userTextField.text = user["name"] as? String
+        // Update other fields similarly
+        // For example:
+        aboutTextField.text = user["bio"] as? String
+        FromAge.text = "\(user["fromage"] as? Int ?? 0)"
+        ToAge.text = "\(user["toage"] as? Int ?? 0)"
+        
+        // Update date picker if user has birthdate
+        if let birthdateString = user["birthdate"] as? String,
+           let birthdate = dateFormatter.date(from: birthdateString) {
+            dobPicker.date = birthdate
         }
+
+        // Update gender picker if user has gender information
+        if let genderString = user["gender"] as? String,
+           let genderIndex = genderData.firstIndex(of: genderString) {
+            genderPicker.selectRow(genderIndex, inComponent: 0, animated: true)
+        }
+
+        // Update campus picker if user has campus information
+        if let campusString = user["campus"] as? String,
+           let campusIndex = campusOptions.firstIndex(of: campusString) {
+            campusPicker.selectRow(campusIndex, inComponent: 0, animated: true)
+        }
+    }
 
 
     
